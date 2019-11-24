fc417ffae7cb32d8ba859f6e19e439b7fba451aa
Manuel Morote <mmh30@alu.ua.es>
2019-11-24 03:46:45 +0100

    Agenda

--- FrontEnd/src/app/pages/agenda/agenda.page.ts
+++ FrontEnd/src/app/pages/agenda/agenda.page.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+class Event {
+  title:string
+  description:string
+  startTime:string
+  endTime:string
+  allDay:boolean
+  constructor(){
+    this.title=""
+    this.description=""
+    this.startTime = new Date().toISOString()
+    this.endTime = new Date().toISOString()
+    this.allDay=false
+  }
+}
+
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.page.html',
@@ -7,9 +22,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendaPage implements OnInit {
 
-  constructor() { }
+  event:Event;
+  eventSource = [];
 
-  ngOnInit() {
+  calendar = {
+    mode:'week',
+    currentDate: new Date(),
+    locale:'es-ES'
   }
 
+  fechaSeleccionada = ""
+  constructor() { }
+
+  changeMode(mode){
+    this.calendar.mode=mode;
+  }
+  ngOnInit() {
+    this.event = new Event()
+  }
+
+  onCurrentDateChanged(){}
+  reloadSource(){}
+  onEventSelected(){}
+  onViewTitleChanged(event){
+    this.fechaSeleccionada = event
+  }
+  onTimeSelected(event){
+  }
 }
