2c86b371085a8bef6b68b21602cb65b100686405
Manuel Morote <mmh30@alu.ua.es>
2020-02-12 18:55:47 +0100

    version 9 of Angular

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -3,20 +3,51 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AlumnoGuard } from './guards/alumno.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' },
-  { path: 'announcements', loadChildren: './pages/announcements/announcements.module#AnnouncementsPageModule', canActivate: [AlumnoGuard] },
-  { path: 'materiales', loadChildren: './pages/materiales/materiales.module#MaterialesPageModule', canActivate: [AlumnoGuard] },
-  { path: 'tutoring', loadChildren: './pages/tutoring/tutoring.module#TutoringPageModule', canActivate: [AlumnoGuard] },
-  { path: 'calendar', loadChildren: './pages/calendar/calendar.module#CalendarPageModule', canActivate: [AlumnoGuard] },
-  { path: 'ratings', loadChildren: './pages/ratings/ratings.module#RatingsPageModule', canActivate: [AlumnoGuard] },
-  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule', canActivate: [AlumnoGuard] },
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/login/login.module').then(m => m.LoginPageModule) },
+  {
+    path: 'announcements',
+    loadChildren: () => import('./pages/announcements/announcements.module').then(m => m.AnnouncementsPageModule),
+    canActivate: [AlumnoGuard] },
+  {
+    path: 'materiales',
+    loadChildren: () => import('./pages/materiales/materiales.module').then(m => m.MaterialesPageModule),
+    canActivate: [AlumnoGuard] },
+  {
+    path: 'tutoring',
+    loadChildren: () => import('./pages/tutoring/tutoring.module').then(m => m.TutoringPageModule),
+    canActivate: [AlumnoGuard] },
+  {
+    path: 'calendar',
+    loadChildren: () => import('./pages/calendar/calendar.module').then(m => m.CalendarPageModule),
+    canActivate: [AlumnoGuard] },
+  {
+    path: 'ratings',
+    loadChildren: () => import('./pages/ratings/ratings.module').then(m => m.RatingsPageModule),
+    canActivate: [AlumnoGuard] },
+  {
+    path: 'settings',
+    loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsPageModule),
+    canActivate: [AlumnoGuard]
+  },
 
   // ASIGNATURAS
-  { path: 'asignaturas', loadChildren: './pages/asignaturas/asignaturas.module#AsignaturasPageModule' },
-  { path: 'perfil', loadChildren: './pages/perfil/perfil.module#PerfilPageModule' },
-  { path: 'usuarios', loadChildren: './pages/usuarios/usuarios.module#UsuariosPageModule' },
-  { path: '**', loadChildren: './pages/not-found404/not-found404.module#NotFound404PageModule' }
+  {
+    path: 'asignaturas',
+    loadChildren: () => import('./pages/asignaturas/asignaturas.module').then(m => m.AsignaturasPageModule) },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then(m => m.PerfilPageModule) },
+  {
+    path: 'usuarios',
+    loadChildren: () => import('./pages/usuarios/usuarios.module').then(m => m.UsuariosPageModule) },
+  {
+    path: '**',
+    loadChildren: () => import('./pages/not-found404/not-found404.module').then(m => m.NotFound404PageModule)
+  }
 
 
 ];
